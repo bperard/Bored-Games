898980d90defcbdbdf423c3cfaadb24380f18617
Brandon Perard <Brandon.Perard@Gmail.com>
2023-09-26 21:03:25 -0700

    ADD turn()

--- games.js
+++ games.js
@@ -8,4 +8,14 @@ class Game {
   shuffle(unshuffledArray) {
     // return shuffledArray;
   }
+
+  turn(time, takeTurnCallback) {
+    setTimeout(takeTurnCallback, time);
+    // timeOut utilize state to render turn component until timeout changes state
+    // if turn is complete before timeout, clear timeout after turn functions are complete
+    // decide mechanism for next turn
+    // - could use a return to indicate game state for other actions
+    // - could trigger inernally, but game needs knowledge of this higher up
+    // - could be integrated into higher order flow, single function here most likely best option
+  }
 }
